5658f937d878e831a88a7a9ff0d785efdbb42865
Pranav Avva <pranav.avva@gmail.com>
2020-08-12 15:23:04 -0400

    Feat/type (#6)
    * Remove unneeded Library class. Add Type enum.
    
    * add package to Type.java

---
+++ src/main/java/io/github/pranavavva/pokejava/api/Type.java
@@ -0,0 +1,22 @@
+package io.github.pranavavva.pokejava.api;
+
+public enum Type {
+    NORMAL,
+    FIGHT,
+    FLYING,
+    POISON,
+    GROUND,
+    ROCK,
+    BUG,
+    GHOST,
+    STEEL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    PSYCHIC,
+    ICE,
+    DRAGON,
+    DARK,
+    FAIRY;
+}
