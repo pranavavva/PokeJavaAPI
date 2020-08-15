627670f0a975ce9373c3b58ea74ba7608947f031
Pranav Avva <pranav.avva@gmail.com>
2020-08-15 15:08:24 -0400

    Feat/docs (#14)
    * Docs: Type
    
    * Docs: Pokemon
    
    * Docs: Move
    
    * Docs: MoveCategory

--- src/main/java/io/github/pranavavva/pokejava/api/MoveCategory.java
+++ src/main/java/io/github/pranavavva/pokejava/api/MoveCategory.java
@@ -1,7 +1,26 @@
 package io.github.pranavavva.pokejava.api;
 
+/**
+ * Determines which set of stats are used in damage calculation. PHYSICAL Moves
+ * use the {@code attack} and {@code defense} stats of the Pokemon. SPECIAL
+ * Moves use the {@code spAttack} and {@code spDefense} stats of the Pokemon.
+ * STATUS Moves do not inflict damage, but they may impart a status effect.
+ */
 public enum MoveCategory {
+    /**
+     * PHYSICAL Moves use the {@code attack} and {@code defense} stats of the
+     * Pokemon.
+     */
     PHYSICAL,
+
+    /**
+     * SPECIAL Moves use the {@code spAttack} and {@code spDefense} stats of the
+     * Pokemon.
+     */
     SPECIAL,
+    
+    /**
+     * STATUS Moves do not inflict damage, but they may impart a status effect.
+     */
     STATUS;
 }
--- src/main/java/io/github/pranavavva/pokejava/api/Type.java
+++ src/main/java/io/github/pranavavva/pokejava/api/Type.java
@@ -1,22 +1,17 @@
 package io.github.pranavavva.pokejava.api;
 
+/**
+ * Defines the combat type of a Pokemon or Move. Pokemon can have one or two
+ * Types, whereas Moves only have one Type. Type is used to determine attack
+ * damage, Pokemon whose Type(s) are resistant to an attacking Move's Type
+ * receive less damage than normal. Pokemon whose Type(s) are weak to an
+ * attacking Move's Type receive more damage than normal. When attacking, a Move
+ * used by a Pokemon who has a matching Type deals more damage.
+ * 
+ * @see Pokemon
+ * @see Move
+ */
 public enum Type {
-    NORMAL,
-    FIGHT,
-    FLYING,
-    POISON,
-    GROUND,
-    ROCK,
-    BUG,
-    GHOST,
-    STEEL,
-    FIRE,
-    WATER,
-    GRASS,
-    ELECTRIC,
-    PSYCHIC,
-    ICE,
-    DRAGON,
-    DARK,
-    FAIRY;
+    NORMAL, FIGHT, FLYING, POISON, GROUND, ROCK, BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC, PSYCHIC, ICE, DRAGON,
+    DARK, FAIRY;
 }
